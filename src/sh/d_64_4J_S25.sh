98236bd5a186127ec16c0d5aee1804ca6c134cfe
Alice Martin <alice.martindonati@gmail.com>
2021-04-19 13:33:03 +0200

    slurm script

--- src/sh/d_64_4J_S25.sh
+++ src/sh/d_64_4J_S25.sh
@@ -17,14 +17,14 @@ conda activate back-is
 
 export PYTHONPATH=src:${PYTHONPATH}
 
-DATA_PATH = "output/RNN_weather/RNN_h32_ep15_bs64_maxsamples20000/20210417-080320/observations_samples1_seqlen25_sigmainit0.1_sigmah0.1_sigmay0.1"
-MODEL_PATH = "output/RNN_weather/RNN_h32_ep15_bs64_maxsamples20000/20210417-080320/model.pt"
-SIGMA_INIT = 0.1
-SIGMA_Y = 0.1
-SIGMA_H = 0.1
-NUM_PARTICLES = 1000
-BACKWARD_SAMPLES = 4
-RUNS = 100
+DATA_PATH="output/RNN_weather/RNN_h32_ep15_bs64_maxsamples20000/20210417-080320/observations_samples1_seqlen25_sigmainit0.1_sigmah0.1_sigmay0.1"
+MODEL_PATH="output/RNN_weather/RNN_h32_ep15_bs64_maxsamples20000/20210417-080320/model.pt"
+SIGMA_INIT=0.1
+SIGMA_Y=0.1
+SIGMA_H=0.1
+NUM_PARTICLES=1000
+BACKWARD_SAMPLES=4
+RUNS=100
 
 set -x
 srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS
