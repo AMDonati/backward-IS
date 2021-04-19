18ec612d2359d01d00f93e845b732cc49d60f5a1
Alice Martin <alice.martindonati@gmail.com>
2021-04-19 22:11:31 +0200

    slurm

--- src/sh/h32_S100.sh
+++ src/sh/h32_S100.sh
@@ -31,5 +31,5 @@ PMS=0
 
 set -x
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
-srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms PMS
+srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms $PMS
 exit 0
--- src/sh/h32_S24.sh
+++ src/sh/h32_S24.sh
@@ -27,9 +27,8 @@ SIGMA_H=0.1
 NUM_PARTICLES=1000
 BACKWARD_SAMPLES=32
 RUNS=1
-PMS=0
 
 set -x
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
-srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms PMS
+srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms $PMS
 exit 0
