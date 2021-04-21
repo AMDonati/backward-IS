65c1869d4206fc8320ae6d5e832ad2f86690655b
Alice Martin <alice.martindonati@gmail.com>
2021-04-21 07:21:44 +0200

    slurm

--- src/sh/OLD/h32_S100.sh
+++ src/sh/OLD/h32_S100.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --array=1-100
+#SBATCH --array=1-50
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8g
 #SBATCH --output=slurm_out_2/h32-S100-%j.out
@@ -27,10 +27,9 @@ SIGMA_H=0.1
 NUM_PARTICLES=1000
 BACKWARD_SAMPLES=32
 RUNS=1
-PMS=1
-PARTICLES_PMS=12000
+PMS=0
 
 set -x
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
-srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms $PMS -particles_pms $PARTICLES_PMS
+srun python -u src/estimate.py -data_path $DATA_PATH -model_path $MODEL_PATH -out_path ${SLURM_ARRAY_TASK_ID} -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -sigma_init $SIGMA_INIT -sigma_y $SIGMA_Y -sigma_h $SIGMA_H -runs $RUNS -pms $PMS
 exit 0
