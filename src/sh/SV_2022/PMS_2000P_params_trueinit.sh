f5ea66e8038ca43ad9591b5aca2d7a0e4aea905c
AMDonati <alice.martindonati@gmail.com>
2022-06-02 03:14:50 +0200

    sh

---
+++ src/sh/SV_2022/PMS_2000P_params_trueinit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=PMS-params-2000P-trueinit
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=12
+#SBATCH --output=slurm_out/PMS-params-2000p-trueinit-%j.out
+#SBATCH --error=slurm_out/PMS-params-2000p-trueinit-%j.err
+#SBATCH --time=100:00:00
+#SBATCH -A ktz@v100
+
+
+export TMPDIR=$JOBSCRATCH
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate back-is
+
+export PYTHONPATH=src:${PYTHONPATH}
+
+DATA_PATH="data/SV"
+OUT_PATH="experiments"
+NUM_PARTICLES=2000
+BACKWARD_SAMPLES=16
+ALGO="PMS"
+N_ITER=50
+INIT_PARAMS="true"
+ESTIM="parameter"
+SEQ_LEN=100
+
+srun python -u src/train/EM_algo.py -data_path $DATA_PATH -out_path $OUT_PATH -num_particles $NUM_PARTICLES -backward_samples $BACKWARD_SAMPLES -algo $ALGO -n_iter $N_ITER -init_params $INIT_PARAMS -estim $ESTIM -seq_len $SEQ_LEN
