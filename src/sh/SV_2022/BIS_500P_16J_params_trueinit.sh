7e7766c5e10990bc2a727ce8be7be0e1c1c5d62c
AMDonati <alice.martindonati@gmail.com>
2022-06-02 03:35:36 +0200

    sh

--- src/sh/SV_2022/BIS_500P_16J_params_trueinit.sh
+++ src/sh/SV_2022/BIS_500P_16J_params_trueinit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=BIS-params-500P-randinit
-#SBATCH --qos=qos_gpu-t3
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --output=slurm_out/BIS-params-550p-trueinit-%j.out
 #SBATCH --error=slurm_out/-%j.err
-#SBATCH --time=20:00:00
+#SBATCH --time=100:00:00
 #SBATCH -A ktz@v100
 
 
